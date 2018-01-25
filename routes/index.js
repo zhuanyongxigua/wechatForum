15c5bf405c7a83d160698efa103ea93a50c1da06
yuxin <yx18513839223@gmail.com>
2018-01-25 23:04:39 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    // res.sendFile('views/001-home.html');
+});
+
+module.exports = router;
