1cb542543cc122763ef08ee0a7bbc2e063fd2d5e
zhuanyongxigua <zhuanyongxigua@gmail.com>
2018-12-07 18:00:06 +0800

    start to handle the webpack

--- views/config/dev.env.js
+++ views/config/dev.env.js
@@ -2,10 +2,6 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
-console.log(merge(prodEnv, {
-  NODE_ENV: '"development"'
-}));
-
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"'
 })
